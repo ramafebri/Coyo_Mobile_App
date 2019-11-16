518cde49f5a43fc50609ac5e3f29f7208afb493b
ramafebri <38251530+ramafebri@users.noreply.github.com>
2019-11-17 02:13:03 +0700

    Merge branch 'master' of C:\Users\Public\Documents\AndroidStudioProject\CoyoMobileApp with conflicts.

--- app/src/main/java/com/example/coyomobileapp/View/Activity/DaftarActivity.java
+++ app/src/main/java/com/example/coyomobileapp/View/Activity/DaftarActivity.java
@@ -1,5 +1,6 @@
 package com.example.coyomobileapp.View.Activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.coyomobileapp.R;
@@ -21,21 +24,20 @@ import org.json.JSONObject;
 
 public class DaftarActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnkeLogin, btnLangsungkeLogin, btnKembali;
-    EditText email, pass, confirmPass;
-    String user, password, confirmPassword;
+    EditText emails, passs, confirmPass;
+    String email, pass, konfirmasiPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
-
-        Firebase.setAndroidContext(this);
         initView();
+        Firebase.setAndroidContext(this);
         click();
     }
 
     public void initView(){
-        email = findViewById(R.id.edtEmail);
-        pass = findViewById(R.id.edtPass);
+        emails = findViewById(R.id.edtEmail);
+        passs = findViewById(R.id.edtPass);
         confirmPass = findViewById(R.id.edtConPass);
         btnLangsungkeLogin = findViewById(R.id.buttonKeLogin1);
         btnKembali = findViewById(R.id.buttonKembali);
@@ -47,83 +49,82 @@ public class DaftarActivity extends AppCompatActivity implements View.OnClickLis
         btnKembali.setOnClickListener(this);
         btnkeLogin.setOnClickListener(this);
     }
+    private Boolean validation(){
+        email = emails.getText().toString();
+        pass = passs.getText().toString();
+        konfirmasiPass = confirmPass.getText().toString();
+
+        if(email.equals("")){
+            emails.setError("can't be blank");
+            return false;
+        }
+        else if(pass.equals("")){
+            passs.setError("can't be blank");
+            return false;
+        }
+        else if (!konfirmasiPass.equals(pass)){
+            confirmPass.setError("Harus sama dengan password");
+        }
+        return true;
+    }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.buttonKeLogin){
+        int i = v.getId();
+        if (i == R.id.btnKembali) {
+            Intent move = new Intent(DaftarActivity.this, StartActivity.class);
+            startActivity(move);
+            finish();
+        } else if (i == R.id.buttonKeLogin) {
             if (validation()){
+                final ProgressDialog pd = new ProgressDialog(DaftarActivity.this);
+                pd.setMessage("Loading...");
+                pd.show();
+
                 String url = "https://coyomobileapp.firebaseio.com/users.json";
 
-                StringRequest request = new StringRequest(Request.Method.GET, url, s -> {
-                    Firebase reference = new Firebase("https://coyomobileapp.firebaseio.com/users");
+                StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String s) {
+                        Firebase reference = new Firebase("https://coyomobileapp.firebaseio.com/users");
 
-                    if(s.equals("null")) {
-                        reference.child(user).child("password").setValue(pass);
-                        Toast.makeText(DaftarActivity.this, "registration successful", Toast.LENGTH_LONG).show();
-                        Intent move = new Intent(DaftarActivity.this, LoginActivity.class);
-                        startActivity(move);
-                    }
-                    else {
-                        try {
-                            JSONObject obj = new JSONObject(s);
-
-                            if (!obj.has(user)) {
-                                reference.child(user).child("password").setValue(pass);
-                                Toast.makeText(DaftarActivity.this, "registration successful", Toast.LENGTH_LONG).show();
-                                Intent move = new Intent(DaftarActivity.this, LoginActivity.class);
-                                startActivity(move);
-                            } else {
-                                Toast.makeText(DaftarActivity.this, "username already exists", Toast.LENGTH_LONG).show();
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                        if(s.equals("null")) {
+                            reference.child(email).child("password").setValue(pass);
+                            Toast.makeText(DaftarActivity.this, "registration successful", Toast.LENGTH_LONG).show();
                         }
-                    }
-                }, volleyError -> System.out.println("" + volleyError ));
+                        else {
+                            try {
+                                JSONObject obj = new JSONObject(s);
 
+                                if (!obj.has(email)) {
+                                    reference.child(email).child("password").setValue(pass);
+                                    Toast.makeText(DaftarActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(DaftarActivity.this, "username already exists", Toast.LENGTH_LONG).show();
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        pd.dismiss();
+                    }
+                },new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        System.out.println("" + volleyError );
+                        pd.dismiss();
+                    }
+                });
                 RequestQueue rQueue = Volley.newRequestQueue(DaftarActivity.this);
                 rQueue.add(request);
             }
 
         }
-        else if (v.getId() == R.id.buttonKeLogin1){
+        else if (i == R.id.buttonKeLogin1){
             Intent move = new Intent(DaftarActivity.this, LoginActivity.class);
             startActivity(move);
-        }
-        else if (v.getId() == R.id.buttonKembali){
-            Intent move = new Intent(DaftarActivity.this,StartActivity.class);
-            startActivity(move);
+            finish();
         }
     }
-
-    private Boolean validation(){
-        user = email.getText().toString();
-        password = pass.getText().toString();
-        confirmPassword = confirmPass.getText().toString();
-
-        if(user.equals("")){
-            email.setError("can't be blank");
-            return false;
-        }
-        else if(password.equals("")){
-            pass.setError("can't be blank");
-            return false;
-        }
-        else if(user.length()<5){
-            email.setError("at least 5 characters long");
-            return false;
-        }
-        else if(password.length()<5){
-            pass.setError("at least 5 characters long");
-            return false;
-        }
-        else if (!confirmPassword.equals(password)){
-            confirmPass.setError("Password konfirmasi harus sama");
-            return false;
-        }
-        return true;
-    }
-
-
 }
