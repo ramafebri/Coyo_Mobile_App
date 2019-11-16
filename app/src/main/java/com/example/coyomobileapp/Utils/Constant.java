64931d842062c95d8eb6d1f29a4000f12dcaff03
luqmansen <luqmansen@gmail.com>
2019-11-17 04:45:31 +0700

    change base url

--- app/src/main/java/com/example/coyomobileapp/Utils/Constant.java
+++ app/src/main/java/com/example/coyomobileapp/Utils/Constant.java
@@ -1,8 +1,8 @@
 package com.example.coyomobileapp.Utils;
 
 public class Constant {
-//    public static final String BASE_URL = "https://coyo-rest.herokuapp.com/kta/";
-    public static final String BASE_URL = "http://10.0.2.2:8080/kta/";
+    public static final String BASE_URL = "https://coyo-rest.herokuapp.com/kta/";
+//    public static final String BASE_URL = "http://10.0.2.2:8080/kta/";
 
     public final class Extra {
         public static final String DATA = "EXTRA_DATA";
