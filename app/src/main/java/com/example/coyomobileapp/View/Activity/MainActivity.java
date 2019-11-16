31722c6ee65b9816140c2d42725983652f75f761
ramafebri <38251530+ramafebri@users.noreply.github.com>
2019-11-16 09:59:47 +0700

    tambahin package semua directory

--- app/src/main/java/com/example/coyomobileapp/View/Activity/MainActivity.java
+++ app/src/main/java/com/example/coyomobileapp/View/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.coyomobileapp;
+package com.example.coyomobileapp.View.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.coyomobileapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
