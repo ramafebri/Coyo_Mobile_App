6a60c4e2fb4760d344f14bc67230f87b6513aa7a
arafasaka <38359952+arafasaka@users.noreply.github.com>
2019-11-16 12:56:53 +0700

    nambah aset2

--- app/src/main/java/com/example/coyomobileapp/View/Activity/Kta2Activity.java
+++ app/src/main/java/com/example/coyomobileapp/View/Activity/Kta2Activity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.coyomobileapp.R;
 
-public class CompareActivity extends AppCompatActivity {
+public class Kta2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compare);
+        setContentView(R.layout.activity_kta2);
     }
 }
